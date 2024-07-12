7b604404f39446d414e920eb13852f7d88d3538e
Sabin Shrestha <sabin.shrestha.er@gmail.com>
2024-07-12 23:09:16 +0545

    seperate array for teamcard constant file

---
+++ src/constants/teamCard.js
@@ -0,0 +1,130 @@
+import { member1, member2 } from "../assets/index";
+
+export const teamCard = [
+  {
+    imgURL: member1,
+    name: "Chris Sprague",
+    position: "CEO",
+    about:
+      "Chris drives the culture, delivery, and healthcare divisions at Leapfrog. He has a vast experience spanning 23 years, from leading enterprise innovation teams at Oracle to being CEO and co-founder of a successful venture-backed startup. He graduated from Stanford University in 2002 with degrees in Symbolic Systems and Computer Science, focusing on Human-Computer Interaction.",
+  },
+  {
+    imgURL: member2,
+    name: "Himal Karmacharya",
+    position: "President",
+    about:
+      "Himal founded Leapfrog in 2010 and leads global growth and operations. An engineer by training, Himal has over 25 years of experience spanning enterprise, healthcare, and data. Himal is an accomplished business leader and speaker. Himal has previously served in leadership roles at Verisk Health, Oracle, and Motorola. He did his bachelor’s and master's in EECS at MIT.",
+  },
+  {
+    imgURL: member1,
+    name: "Chris Sprague",
+    position: "CEO",
+    about:
+      "Chris drives the culture, delivery, and healthcare divisions at Leapfrog. He has a vast experience spanning 23 years, from leading enterprise innovation teams at Oracle to being CEO and co-founder of a successful venture-backed startup. He graduated from Stanford University in 2002 with degrees in Symbolic Systems and Computer Science, focusing on Human-Computer Interaction.",
+  },
+  {
+    imgURL: member2,
+    name: "Himal Karmacharya",
+    position: "President",
+    about:
+      "Himal founded Leapfrog in 2010 and leads global growth and operations. An engineer by training, Himal has over 25 years of experience spanning enterprise, healthcare, and data. Himal is an accomplished business leader and speaker. Himal has previously served in leadership roles at Verisk Health, Oracle, and Motorola. He did his bachelor’s and master's in EECS at MIT.",
+  },
+  {
+    imgURL: member1,
+    name: "Chris Sprague",
+    position: "CEO",
+    about:
+      "Chris drives the culture, delivery, and healthcare divisions at Leapfrog. He has a vast experience spanning 23 years, from leading enterprise innovation teams at Oracle to being CEO and co-founder of a successful venture-backed startup. He graduated from Stanford University in 2002 with degrees in Symbolic Systems and Computer Science, focusing on Human-Computer Interaction.",
+  },
+  {
+    imgURL: member2,
+    name: "Himal Karmacharya",
+    position: "President",
+    about:
+      "Himal founded Leapfrog in 2010 and leads global growth and operations. An engineer by training, Himal has over 25 years of experience spanning enterprise, healthcare, and data. Himal is an accomplished business leader and speaker. Himal has previously served in leadership roles at Verisk Health, Oracle, and Motorola. He did his bachelor’s and master's in EECS at MIT.",
+  },
+  {
+    imgURL: member1,
+    name: "Chris Sprague",
+    position: "CEO",
+    about:
+      "Chris drives the culture, delivery, and healthcare divisions at Leapfrog. He has a vast experience spanning 23 years, from leading enterprise innovation teams at Oracle to being CEO and co-founder of a successful venture-backed startup. He graduated from Stanford University in 2002 with degrees in Symbolic Systems and Computer Science, focusing on Human-Computer Interaction.",
+  },
+  {
+    imgURL: member2,
+    name: "Himal Karmacharya",
+    position: "President",
+    about:
+      "Himal founded Leapfrog in 2010 and leads global growth and operations. An engineer by training, Himal has over 25 years of experience spanning enterprise, healthcare, and data. Himal is an accomplished business leader and speaker. Himal has previously served in leadership roles at Verisk Health, Oracle, and Motorola. He did his bachelor’s and master's in EECS at MIT.",
+  },
+  {
+    imgURL: member1,
+    name: "Chris Sprague",
+    position: "CEO",
+    about:
+      "Chris drives the culture, delivery, and healthcare divisions at Leapfrog. He has a vast experience spanning 23 years, from leading enterprise innovation teams at Oracle to being CEO and co-founder of a successful venture-backed startup. He graduated from Stanford University in 2002 with degrees in Symbolic Systems and Computer Science, focusing on Human-Computer Interaction.",
+  },
+  {
+    imgURL: member2,
+    name: "Himal Karmacharya",
+    position: "President",
+    about:
+      "Himal founded Leapfrog in 2010 and leads global growth and operations. An engineer by training, Himal has over 25 years of experience spanning enterprise, healthcare, and data. Himal is an accomplished business leader and speaker. Himal has previously served in leadership roles at Verisk Health, Oracle, and Motorola. He did his bachelor’s and master's in EECS at MIT.",
+  },
+  {
+    imgURL: member1,
+    name: "Chris Sprague",
+    position: "CEO",
+    about:
+      "Chris drives the culture, delivery, and healthcare divisions at Leapfrog. He has a vast experience spanning 23 years, from leading enterprise innovation teams at Oracle to being CEO and co-founder of a successful venture-backed startup. He graduated from Stanford University in 2002 with degrees in Symbolic Systems and Computer Science, focusing on Human-Computer Interaction.",
+  },
+  {
+    imgURL: member2,
+    name: "Himal Karmacharya",
+    position: "President",
+    about:
+      "Himal founded Leapfrog in 2010 and leads global growth and operations. An engineer by training, Himal has over 25 years of experience spanning enterprise, healthcare, and data. Himal is an accomplished business leader and speaker. Himal has previously served in leadership roles at Verisk Health, Oracle, and Motorola. He did his bachelor’s and master's in EECS at MIT.",
+  },
+  {
+    imgURL: member1,
+    name: "Chris Sprague",
+    position: "CEO",
+    about:
+      "Chris drives the culture, delivery, and healthcare divisions at Leapfrog. He has a vast experience spanning 23 years, from leading enterprise innovation teams at Oracle to being CEO and co-founder of a successful venture-backed startup. He graduated from Stanford University in 2002 with degrees in Symbolic Systems and Computer Science, focusing on Human-Computer Interaction.",
+  },
+  {
+    imgURL: member2,
+    name: "Himal Karmacharya",
+    position: "President",
+    about:
+      "Himal founded Leapfrog in 2010 and leads global growth and operations. An engineer by training, Himal has over 25 years of experience spanning enterprise, healthcare, and data. Himal is an accomplished business leader and speaker. Himal has previously served in leadership roles at Verisk Health, Oracle, and Motorola. He did his bachelor’s and master's in EECS at MIT.",
+  },
+  {
+    imgURL: member1,
+    name: "Chris Sprague",
+    position: "CEO",
+    about:
+      "Chris drives the culture, delivery, and healthcare divisions at Leapfrog. He has a vast experience spanning 23 years, from leading enterprise innovation teams at Oracle to being CEO and co-founder of a successful venture-backed startup. He graduated from Stanford University in 2002 with degrees in Symbolic Systems and Computer Science, focusing on Human-Computer Interaction.",
+  },
+  {
+    imgURL: member2,
+    name: "Himal Karmacharya",
+    position: "President",
+    about:
+      "Himal founded Leapfrog in 2010 and leads global growth and operations. An engineer by training, Himal has over 25 years of experience spanning enterprise, healthcare, and data. Himal is an accomplished business leader and speaker. Himal has previously served in leadership roles at Verisk Health, Oracle, and Motorola. He did his bachelor’s and master's in EECS at MIT.",
+  },
+  {
+    imgURL: member1,
+    name: "Chris Sprague",
+    position: "CEO",
+    about:
+      "Chris drives the culture, delivery, and healthcare divisions at Leapfrog. He has a vast experience spanning 23 years, from leading enterprise innovation teams at Oracle to being CEO and co-founder of a successful venture-backed startup. He graduated from Stanford University in 2002 with degrees in Symbolic Systems and Computer Science, focusing on Human-Computer Interaction.",
+  },
+  {
+    imgURL: member2,
+    name: "Himal Karmacharya",
+    position: "President",
+    about:
+      "Himal founded Leapfrog in 2010 and leads global growth and operations. An engineer by training, Himal has over 25 years of experience spanning enterprise, healthcare, and data. Himal is an accomplished business leader and speaker. Himal has previously served in leadership roles at Verisk Health, Oracle, and Motorola. He did his bachelor’s and master's in EECS at MIT.",
+  },
+];
